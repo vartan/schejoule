8ad17010b97b7f3fe85cd6ef3ad07d88333e7c6b
Michael Vartan <admin@mvartan.com>
2014-01-16 22:18:15 -0800

    scraping data into json

--- scrapeCSULB.py
+++ scrapeCSULB.py
@@ -1,4 +1,7 @@
 import urllib2
+import json
+import HTMLParser
+h = HTMLParser.HTMLParser()
 from BeautifulSoup import BeautifulSoup
 # or if you're using BeautifulSoup4:
 # from bs4 import BeautifulSoup
@@ -18,21 +21,48 @@ def get_all_department_URLs():
 
 def load_classes(url):
 	soup = BeautifulSoup(urllib2.urlopen(url).read())
-	headers = soup.findAll('div', {'class':'courseHeader'});
-	classes = []
-	for header in headers:
-		courseCode = header.find('span', {'class':'courseCode'}).getText()
-		courseTitle = header.find('span', {'class':'courseTitle'}).getText();
-		classes.append(courseCode+": "+courseTitle);
+	classBlocks = soup.findAll('div', {'class':'courseBlock'});
+	classes = {}
+	for classBlock in classBlocks:
+		courseCode = classBlock.find('span', {'class':'courseCode'}).getText()
+		courseTitle = h.unescape(classBlock.find('span', {'class':'courseTitle'}).getText())
+		thisCourse = {};
+		
+		sections = {};
+		classes[courseCode+": "+courseTitle] = thisCourse;
+		thisCourse["Code"] = courseCode;
+		thisCourse["Title"]= courseTitle;
+		thisCourse["Sections"] = sections;
+		classTables = classBlock.findAll("table");
+		
+		for classTable in classTables:
+			groupRows = classTable.findAll("tr");
+			pastHeader = False
+			labels = ["ID","Notes","Type","Days","Time","Open","Location","Instructor","Comment"];
+			for sectionRow in groupRows:
+				sectionInfo = sectionRow.findAll("td");
+				sectionNum = sectionRow.find("th").getText();
+				thisClass={};
+				if not pastHeader:
+					pastHeader=True;
+					continue
+				for i in range(1, len(labels)):
+					thisClass[labels[i]] = h.unescape(sectionInfo[i].getText())
+				print sectionNum
+				sections[sectionNum] = thisClass;
 	return classes;
-
 departmentClassLists = get_all_department_URLs();
+allClasses = {};
 for departmentClassListURL in departmentClassLists:
 	parsedDept = False;	
 	while not parsedDept:
 		try:
-			classList = load_classes(departmentClassListURL);
-			for className in classList:
-				print className
+			
+			added_classes = load_classes(departmentClassListURL);
+			allClasses.update(added_classes);
+			print json.dumps(added_classes, indent=2);		
 			parsedDept = True;
-		except:pass	
+		except:pass
+fo = open("classes.json", "w")
+fo.write(json.dumps(allClasses,indent=2))
+fo.close()
